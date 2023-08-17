e0182a485f9892a1da5aa152c5252de8d0399fab
William Buerk <wbuerk2@illinois.edu>
2023-08-17 18:56:45 -0500

    Getting rid of base in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/OMDb-API-Demo-React/',
+  // base: '/OMDb-API-Demo-React/',
   plugins: [react()],
 })
